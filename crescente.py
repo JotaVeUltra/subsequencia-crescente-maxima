a24cbf2d7c2a6c3ecaab00508bc0e377d5f861ff
João Marques <jotaveultra@gmail.com>
2019-04-21 22:29:13 -0300

    Dinamico falho

--- crescente.py
+++ crescente.py
@@ -11,6 +11,24 @@ def guloso(sequencia):
     return subsequencia
 
 
+# Tentativa de fazer dinamico
+# Melhor que a função gulosa
+# Não garante o ótimo
+# Falha em [4,5,1,2,3]
+def dinamico(sequencia):
+    subsequencia = [float('inf')]
+    for valor in sequencia:
+        if valor >= subsequencia[-1]:
+            subsequencia.append(valor)
+        else:
+            if len(subsequencia) == 1:
+                subsequencia[-1] = valor
+            elif len(subsequencia) > 1 and subsequencia[-2] <= valor:
+                subsequencia[-1] = valor
+
+    return subsequencia
+
+
 sequencia_aleatoria = list(range(1, 21))
 shuffle(sequencia_aleatoria)
 
@@ -20,4 +38,12 @@ print()
 
 print('guloso')
 print(guloso(sequencia_aleatoria))
+print()
+
+print('dinamico')
+print(dinamico(sequencia_aleatoria))
+print()
+print([4, 5, 1, 2, 3])
+print('ótimo [1, 2, 3]')
+print(dinamico([4, 5, 1, 2, 3]))
 print()
